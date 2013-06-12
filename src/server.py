a6bf00b104980c77e65e2d7234a62b3cf19b0e01
Lexman <lexman@lexman-1018P.(none)>
2013-06-12 14:10:25 +0200

    Web page that queries for a route in kml format

--- src/server.py
+++ src/server.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bottle import route, run
+from bottle import route, run, static_file
+
+@route('/')
+def index():
+    return static_file("index.html", root=".")
+
+@route('/js/<path:path>')
+def js(path):
+    return static_file(path, root='js')
 
 @route('/hello')
 def hello():
