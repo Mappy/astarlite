0e2d70c86f4799531d0219cb053641e0ae87b068
pierre <pierre@ubuntu-VirtualBox.(none)>
2013-06-12 15:55:27 +0200

    plug server and geo functions

--- src/server.py
+++ src/server.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from bottle import route, run, static_file
+from spatialite_routing import Route
 
 @route('/')
 def index():
@@ -14,5 +15,13 @@ def js(path):
 @route('/hello')
 def hello():
     return "Everyday a star is born (Can you say New York City?) Clap for 'em, clap for 'em, clap for 'em, hey"
+
+@route('/route/<start>/<end>')
+def route(start, end):
+	# ex: 7.4218041,43.736974400000001, 7.4186261,43.725380600000001
+	route = Route('/home/pierre/dev/routing.sqlite')
+	lat_from, lng_from = tuple(map(float, start.split(",")))
+	lat_to, lng_to = tuple(map(float, end.split(",")))
+	return route.route(lat_from, lng_from, lat_to, lng_to)
 
 run(host='localhost', port=8080, debug=True)
