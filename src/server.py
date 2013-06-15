53047e69d4dd7253ae84f8591cbec40462d90835
Alexandre Bonnasseau <alexandre.bonnasseau@mappy.com>
2013-06-15 14:15:42 +0200

    Routing database in command line argument for the server

--- src/server.py
+++ src/server.py
@@ -3,8 +3,10 @@
 
 from bottle import route, run, static_file, template
 from spatialite_routing import Route
+import argparse
 
 kml_template = open("template.kml", "r").read()
+dbfile = "routing.sqlite"
 
 @route('/')
 def index():
@@ -20,11 +22,17 @@ def hello():
 
 @route('/route/<start>/<end>')
 def route(start, end):
-	# ex: 7.4218041,43.736974400000001, 7.4186261,43.725380600000001
-	route = Route('/home/pierre/dev/routing.sqlite')
-	lat_from, lng_from = tuple(map(float, start.split(",")))
-	lat_to, lng_to = tuple(map(float, end.split(",")))
-	path = route.route(lat_from, lng_from, lat_to, lng_to)
-	return template(kml_template, path=path)
-		
-run(host='localhost', port=8080, debug=True)
+    # ex: 7.4218041,43.736974400000001, 7.4186261,43.725380600000001
+    route = Route(dbfile)
+    lat_from, lng_from = tuple(map(float, start.split(",")))
+    lat_to, lng_to = tuple(map(float, end.split(",")))
+    path = route.route(lat_from, lng_from, lat_to, lng_to)
+    return template(kml_template, path=path)
+        
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("database")
+    args = parser.parse_args()
+    dbfile = args.database
+    run(host='localhost', port=8080, debug=True)
